6aeda9527caf55a075d53259dffb0c5a3ad4474f
Istieak <istieak.ahmed@yahoo.com>
2023-12-28 17:27:44 +0600

    Commented out PUBLISHABLE_KEY validation

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
+// if (!PUBLISHABLE_KEY) {
+//   throw new Error("Missing Publishable Key")
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
